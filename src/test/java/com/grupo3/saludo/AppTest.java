fccf9a8bf9f740c984e4eea9c80596b7a8d8a440
EdoAbarca <eduardo.abarca.c@usach.cl>
2025-06-11 22:06:02 -0400

    feat: Se crea aplicación java con Maven

---
+++ src/test/java/com/grupo3/saludo/AppTest.java
@@ -0,0 +1,10 @@
+package com.grupo3.saludo;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+public class AppTest {
+    @Test
+    public void testSaludo() {
+        assertEquals("¡Hola, DevOps!", App.saludar("DevOps"));
+    }
+}
